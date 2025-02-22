6714c7e690c392efc750f4626574e01f2f3747f0
Alisson Amaral <aalissonamaral98@gnail.com>
2025-02-22 00:43:48 -0300

    add locator examples

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,21 +1,35 @@
 import test from "@playwright/test";
 
-test.beforeEach(async () => {
-
-})
-
-test('the first test', async ({page}) => {
+test.beforeEach(async ({page}) => {
   await page.goto('http://localhost:4200');
 
-  await page.getByText("forms").click();
-
+  await page.getByText("Forms").click();
   await page.getByText("Form Layouts").click();
+
 })
 
+test.skip('Locator syntax rules', async ({page}) => {
+  // by tag name
+  page.locator('input')
 
-test("navigate to datepicker page", async ({page}) => {
-  await page.goto('http://localhost:4200');
+  // by id
+  page.locator('#inputEmail')
 
-  await page.getByText("forms").click();
+  //by class value
+  page.locator('.shape-rectangle')
 
+  // by attribute
+  page.locator('[placeholder="Email"]')
+
+  // by entire class value
+  page.locator('class="shape-rectangle shape-circle"')
+
+  // combine different selectors without spaces
+  page.locator('input[placeholder="Email"][nbinput]')
+
+  // exact text match
+  page.locator(':text-is("Using the grid")')
+
+  // partial match
+  page.locator(':text("Using")')
 })
